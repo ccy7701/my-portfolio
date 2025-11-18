7874c5aca741e93461a858c6649926cf3c2b85d7
Chiew Cheng Yi <150312205+ccy7701@users.noreply.github.com>
2025-11-18 12:25:49 +0800

    Update +page.svelte

--- src/routes/writing/ums-commemoration/+page.svelte
+++ src/routes/writing/ums-commemoration/+page.svelte
@@ -28,35 +28,35 @@
     </p>
 
     <p>
-      Four years ago, I entered with fear. I was set on becoming a civil engineer up until the final moment because I thought to myself, being the ignoramus I was – I think SWE is something I can be decent at because I like computers. However, I had nary any exposure to computer science (CS) outside of hobbyist pursuits. My fear was that the lack of a formal foundation to CS would make me fall behind.
+      &emsp;&emsp;&emsp;Four years ago, I entered with fear. I was set on becoming a civil engineer up until the final moment because I thought to myself, being the ignoramus I was – I think SWE is something I can be decent at because I like computers. However, I had nary any exposure to computer science (CS) outside of hobbyist pursuits. My fear was that the lack of a formal foundation to CS would make me fall behind.
     </p>
 
     <p>
-      That’s why when I first started the degree, I was very attentive. I took notes, recorded lectures, watched online tutorials and played with code in my free time. When my first semester results came in and I got an SGPA of 3.96, I honestly thought it was a fluke. The next semester I got a 3.92, then a 3.96 after that, after which I figured “oh my, I must be on to something”. So, I kept studying. I didn’t want to look like an impostor.
+      &emsp;&emsp;&emsp;That’s why when I first started the degree, I was very attentive. I took notes, recorded lectures, watched online tutorials and played with code in my free time. When my first semester results came in and I got an SGPA of 3.96, I honestly thought it was a fluke. The next semester I got a 3.92, then a 3.96 after that, after which I figured “oh my, I must be on to something”. So, I kept studying. I didn’t want to look like an impostor.
     </p>
 
     <p>
-      I felt my first year was robbed from me. In contrast to my sister who will experience four full years of the physical campus experience, I lost one year to the pandemic. It is a firm belief of mine that if I got that time, I could know my peers way better than I do now. That said, I’m grateful for the opportunity to make the friends I’ve made along the way. Friends who’ve accepted me for my quirks and oddities.
+      &emsp;&emsp;I felt my first year was robbed from me. In contrast to my sister who will experience four full years of the physical campus experience, I lost one year to the pandemic. It is a firm belief of mine that if I got that time, I could know my peers way better than I do now. That said, I’m grateful for the opportunity to make the friends I’ve made along the way. Friends who’ve accepted me for my quirks and oddities.
     </p>
 
     <p>
-      Several places around the campus are burned into my mind for I’ve made fond memories there. I’m grateful that these memories I’ve made, I’ve made with my friends. The library, where I’ve grinded assignments, exam prep, and killed time. The padang kawad, where we go for tamu to eat snacks after evening classes (some of these classes we join together with due reluctance). The ODEC beach, where we took in the warm sea breeze and salty scent of the air. The lecture halls, where we almost always sat to the middle-left. During the large gaps of time between lectures, we would drive wherever we desired.
+      &emsp;&emsp;Several places around the campus are burned into my mind for I’ve made fond memories there. I’m grateful that these memories I’ve made, I’ve made with my friends. The library, where I’ve grinded assignments, exam prep, and killed time. The padang kawad, where we go for tamu to eat snacks after evening classes (some of these classes we join together with due reluctance). The ODEC beach, where we took in the warm sea breeze and salty scent of the air. The lecture halls, where we almost always sat to the middle-left. During the large gaps of time between lectures, we would drive wherever we desired.
     </p>
 
     <p>
-      Of course, being the introvert I am, I appreciate being able to recharge by myself. I find driving around the campus alone therapeutic. I’ve done so often. I’ve walked many of the concrete paths as well. The pink mosque and chancellery offer a splendid view in the day. The yellowish hue from the streetlights makes the roads look very pretty at night. To me, all of this would always be a welcome respite from the hell that the course inflicted on me.
+      &emsp;&emsp;Of course, being the introvert I am, I appreciate being able to recharge by myself. I find driving around the campus alone therapeutic. I’ve done so often. I’ve walked many of the concrete paths as well. The pink mosque and chancellery offer a splendid view in the day. The yellowish hue from the streetlights makes the roads look very pretty at night. To me, all of this would always be a welcome respite from the hell that the course inflicted on me.
     </p>
 
     <p>
-      I anticipate graduation day with a due sense of sorrow. Perilous as it was, my degree life was systematic. Chaotic yet still foreseeable. I will be walking out of that Chancellor’s hall into an unpredictable future.
+      &emsp;&emsp;I anticipate graduation day with a due sense of sorrow. Perilous as it was, my degree life was systematic. Chaotic yet still foreseeable. I will be walking out of that Chancellor’s hall into an unpredictable future.
     </p>
 
     <p>
-      At the same time, I look forward to graduation day with a right sense of happiness. A tempered happiness. I made it through my perilous four years. Instead of a blue book, I’ll be getting a black book when I step on that stage. At least on that day, I can feel that my efforts were worth it.
+      &emsp;&emsp;At the same time, I look forward to graduation day with a right sense of happiness. A tempered happiness. I made it through my perilous four years. Instead of a blue book, I’ll be getting a black book when I step on that stage. At least on that day, I can feel that my efforts were worth it.
     </p>
 
     <p>
-      Perhaps more important than that, I see graduation day as a day of commemoration. “It takes a village to raise a child”; I use this proverb loosely. For if it weren’t for the institution and the people that raised this person – a participant in the adult world with just the slightest tinge of childlike wonder – I wouldn’t be here today.
+      &emsp;&emsp;Perhaps more important than that, I see graduation day as a day of commemoration. “It takes a village to raise a child”; I use this proverb loosely. For if it weren’t for the institution and the people that raised this person – a participant in the adult world with just the slightest tinge of childlike wonder – I wouldn’t be here today.
     </p>
   </div>
 </article>
@@ -102,6 +102,10 @@
     color: var(--text-muted);
   }
 
+  .content p {
+    margin-bottom: 1.5rem;
+  }
+
   @media (max-width: 900px) {
     .subtitle-date {
       flex-direction: column;
@@ -123,7 +127,7 @@
 
   .image-container {
     text-align: center;
-    margin: 2rem 0;
+    margin: 2rem 0 1.5rem 0;
   }
 
   .image-container img {
